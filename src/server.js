69915a7d8caf9ab4a68177df9cf259a52b850b86
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-06-02 20:40:34 +0700

    Connect MongoDB

--- src/server.js
+++ src/server.js
@@ -1,27 +1,49 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+// eslint-disable-next-line no-unused-vars
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  const hostname = 'localhost'
+  const port = 8017
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    console.log(`3. Hi Van Kiet Dev, Back-end Server is running successfully at Host: http://${hostname}:${port}`)
+  })
+}
+
+// Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên.
+// Inmediately-invoked / Anonymous Async Functions (IIFE)
+(async () => {
+  try {
+    console.log('1. Connect to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connect to MongoDB Cloud Atlas!')
+
+    // Khởi động Server Back-end sau khi Connect Database thành công
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+/* // Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên.
+console.log('1. Connect to MongoDB Cloud Atlas...')
+CONNECT_DB()
+  .then(() => console.log('2. Connect to MongoDB Cloud Atlas!'))
+  .then(() => START_SERVER())
+  // eslint-disable-next-line no-unused-vars
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  }) */
