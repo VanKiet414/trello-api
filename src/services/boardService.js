ebf8488bd6287ba3dc4657ed1f5c748e6697a467
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-06-09 23:36:19 +0700

    Api Create Board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -10,12 +11,18 @@ const createNew = async (reqBody) => {
     }
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ...
+    const createBoard = await boardModel.createNew(newBoard)
+    console.log(createBoard)
+
+    // Lấy bản ghi board sau khi gọi ( tùy mục đích dự án có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createBoard.insertedId)
+    console.log(getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Collection khác tuỳ đặc thù dự án...vv
     // Bắn email, notification về cho admin khi có 1 cái board mới được tạo...vv
 
-    return newBoard // Trả về kết quả về trong tầng Service luôn phải có return
+    // Trả về kết quả về trong tầng Service luôn phải có return
+    return getNewBoard
   } catch (error) { throw error }
 }
 
