93dcbd10a1ed3c430c4e7048ec4be8e2d30b10a3
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-06-12 23:20:23 +0700

    MongoDB Aggregate

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { BOARD_TYPES } from '~/utils/constants'
 
 const createNew = async (req, res, next) => {
   /**
@@ -25,7 +26,8 @@ const createNew = async (req, res, next) => {
       'string.max': 'Title must be less than or equal to 5 characters long (vankietdev)',
       'string.trim': 'Title must not have leading or trailing whitespace (vankietdev)'
     }),
-    description: Joi.string().min(3).max(256).required().trim().strict()
+    description: Joi.string().min(3).max(256).required().trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required()
   })
 
   try {
