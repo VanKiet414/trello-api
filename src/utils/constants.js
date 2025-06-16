2624bfab2434994454b3e80d345de4e0e6dba492
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-06-16 22:03:44 +0700

    Update CORS not allow localhost.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những cái domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
+  // ...vv vì sau này sẽ deploy lên domain chính thức ...vv
 ]
 
 export const BOARD_TYPES = {
