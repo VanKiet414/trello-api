f21f57396be590738317758d0dfd971c198928e3
vankietdev <vvkiet.12c3tcnvn2023@gmail.com>
2025-07-23 00:03:50 +0700

    feat: Add new feature create card

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,8 @@ import { env } from '~/config/environment'
 
 // Những cái domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173', // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
   'https://trello-web-zz9r.vercel.app',
   'https://vankietdev.io.vn'
-  // ...vv vì sau này sẽ deploy lên domain chính thức ...vv
 ]
 
 export const BOARD_TYPES = {
